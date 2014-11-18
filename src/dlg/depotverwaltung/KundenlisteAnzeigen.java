00d533f99dd2bbe883e7d5a2fe6d35bc76e56971
Philip Dauwe <philip@pdauwe.de>
2014-11-18 09:40:19 +0100

    Kundenliste Windows Dispose statt Exit

--- src/dlg/depotverwaltung/KundenlisteAnzeigen.java
+++ src/dlg/depotverwaltung/KundenlisteAnzeigen.java
@@ -40,7 +40,7 @@ public class KundenlisteAnzeigen extends JFrame {
 	 * Create the frame.
 	 */
 	public KundenlisteAnzeigen(IKundenlisteErstellenRemote kundenlisteErstellenRemote) {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
